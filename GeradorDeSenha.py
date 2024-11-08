2791226853a4a83509ea9f08dbb6e0cfe8bbae2f
Cadu Passaes <kadugrupassaes@gmail.com>
2024-11-08 20:31:25 -0300

    Input ainda dando problema mas as senhas ja estão  sendo geradas corretamente

--- GeradorDeSenha.py
+++ GeradorDeSenha.py
@@ -5,8 +5,11 @@ carac = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%()&1234567890'
 
 
 def pegaQtdTam():
+    
     qtd = int(input("Quantas senhas você quer que sejam geradas?"))
     tam = int(input("Qual o tamanho das senhas você quer que sejam geradas?"))
+    
+    
     return [qtd,tam]
     
 
@@ -18,9 +21,8 @@ def geradorDeSenha(dados):
     for l in range(dados[0]):
         senha = ''
         for s in range(dados[1]):
-            senha = random.choice(carac)
-            listaSenhas.append(senha)
-            print("senha")
+            senha += random.choice(carac)
+        listaSenhas.append(senha)
     
     return listaSenhas
 
@@ -30,7 +32,7 @@ print("Bem vindo ao gerador de senhas!!")
 dados = pegaQtdTam()
 print("Suas senhas estão sendo geradas!")
 listaDeSenhas = geradorDeSenha(dados)
-
+print(listaDeSenhas)
 
 
 
